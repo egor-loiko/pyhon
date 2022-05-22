7c8328875f4c30431b87228ff480e3cc2f274393
egor-loiko <eloiko1.21@gmail.com>
2022-05-23 00:04:39 +0300

    add lab 2 task 2_2

---
+++ LAB_PRACTICE/lab_2/lab2_task_2_2.py
@@ -0,0 +1,22 @@
+# 2.	Симметричная матрица.
+# Дана квадратная матрица. Проверить, является ли она симметричной относительно главной диагонали.
+
+import numpy as np
+import random
+
+n = int(input("Введите размерность матрицы: \n"))
+A = np.array([[random.randint(1, 2) for j in range(n)] for i in range(n)])
+print(A)
+simIndicator = True
+for i in range(n):
+    if simIndicator:
+        for j in range(n):
+            if A[i][j] != A[j][i]:
+                simIndicator = False
+                break
+        else:
+            break
+if simIndicator:
+    print("Матрица симмметрична")
+else:
+    print("Матрица НЕ симмметрична")
