57dc7b471afada38648ec8a84373c13056d8184b
egor-loiko <eloiko1.21@gmail.com>
2022-05-31 15:48:50 +0300

    fix lab3 tribonachi task

--- LAB_PRACTICE/lab_3/lab3_task_2_tribanachi.py
+++ LAB_PRACTICE/lab_3/lab3_task_2_tribanachi.py
@@ -7,12 +7,15 @@ def fibonacci(n):
     return cur
 
 
-element = int(input('Введите номер искомого элемента : '))
-value = fibonacci(element)
-print(str(element) + 'й элемент последовательности равен ' + str(value))
+element = int(input('Длину ряда трибоначчи (желательно не более 30 : '))
+if element > 30:
+    print("Слишком длинный ряд, вычисление займет много времени")
+else:
+    value = fibonacci(element)
+    print(str(element) + 'й элемент последовательности равен ' + str(value))
 
-A = []
-for i in range(element):
-    A.append(fibonacci(i + 1))
+    A = []
+    for i in range(element):
+        A.append(fibonacci(i + 1))
 
-print("Ряд трибоначи размером " + str(element) + " элементов\n", A)
+    print("Ряд трибоначи размером " + str(element) + " элементов\n", A)
