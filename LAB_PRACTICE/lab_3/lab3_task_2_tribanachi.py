5f79a1a04605d944991b738e15ff8b8b1c04a4d6
egor-loiko <eloiko1.21@gmail.com>
2022-06-12 08:54:07 +0300

    fix function name

--- LAB_PRACTICE/lab_3/lab3_task_2_tribanachi.py
+++ LAB_PRACTICE/lab_3/lab3_task_2_tribanachi.py
@@ -1,9 +1,9 @@
-def fibonacci(n):
+def tribonacci(n):
     cur = 0
     if n == 3:
         cur = 1
     if n > 3:
-        cur = fibonacci(n - 1) + fibonacci(n - 2) + fibonacci(n - 3)
+        cur = tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3)
     return cur
 
 
@@ -11,11 +11,11 @@ element = int(input('Введите длину ряда трибоначчи (ж
 if element > 30:
     print("Слишком длинный ряд, вычисление займет много времени")
 else:
-    value = fibonacci(element)
+    value = tribonacci(element)
     print(str(element) + 'й элемент последовательности равен ' + str(value))
 
     A = []
     for i in range(element):
-        A.append(fibonacci(i + 1))
+        A.append(tribonacci(i + 1))
 
     print("Ряд трибоначи размером " + str(element) + " элементов\n", A)
