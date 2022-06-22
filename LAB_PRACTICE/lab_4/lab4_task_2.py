cc81209741df9fb4c7e2f7f20b5c960fac00aebf
egor-loiko <eloiko1.21@gmail.com>
2022-06-23 00:28:36 +0300

    Lab 4 task2 incapsulation implementation

--- LAB_PRACTICE/lab_4/lab4_task_2.py
+++ LAB_PRACTICE/lab_4/lab4_task_2.py
@@ -19,15 +19,15 @@ class Book:
     #     self.book_binding_type = book_binding_type
 
     def get_book_id(self):
-        return self.book_id
+        return self.__book_id
 
     def set_book_id(self, book_id):
         if not self.is_book_id_exist(book_id):
-            self.book_id = book_id
+            self.__book_id = book_id
         else:
             print("\33[31m\033[1m {}".format('Same Book ID already exist. Enter another book Id'))
             book_id = int(input("\33[0m {}".format('Enter Book ID: ')))
-            self.book_id = self.set_book_id(book_id)
+            self.__book_id = self.set_book_id(book_id)
         return self.get_book_id()
 
     def is_book_id_exist(self, book_id):
@@ -39,46 +39,46 @@ class Book:
             return False
 
     def get_book_name(self):
-        return self.book_name
+        return self.__book_name
 
     def set_book_name(self, book_name):
-        self.book_name = book_name
+        self.__book_name = book_name
 
     def get_book_author(self):
-        return self.book_author
+        return self.__book_author
 
     def set_book_author(self, book_author):
-        self.book_author = book_author
+        self.__book_author = book_author
 
     def get_book_publishing_house(self):
-        return self.book_publishing_house
+        return self.__book_publishing_house
 
     def set_book_publishing_house(self, book_publishing_house):
-        self.book_publishing_house = book_publishing_house
+        self.__book_publishing_house = book_publishing_house
 
     def get_book_year(self):
-        return self.book_year
+        return self.__book_year
 
     def set_book_year(self, book_year):
-        self.book_year = book_year
+        self.__book_year = book_year
 
     def get_book_pages(self):
-        return self.book_pages
+        return self.__book_pages
 
     def set_book_pages(self, book_pages):
-        self.book_pages = book_pages
+        self.__book_pages = book_pages
 
     def get_book_price(self):
-        return self.book_price
+        return self.__book_price
 
     def set_book_price(self, book_price):
-        self.book_price = book_price
+        self.__book_price = book_price
 
     def get_book_binding_type(self):
-        return self.book_binding_type
+        return self.__book_binding_type
 
     def set_book_binding_type(self, book_binding_type):
-        self.book_binding_type = book_binding_type
+        self.__book_binding_type = book_binding_type
 
     @staticmethod
     def get_book_list():
@@ -93,11 +93,11 @@ class Book:
         checker = 0
         index_store = 0
         for i in range(len(Book.book_list)):
-            if Book.book_list[i].book_id == book_id:
+            if Book.book_list[i].__book_id == book_id:
                 index_store = i
                 checker = 1
         if checker == 0:
-            print('Book with ID = \'' + str(book_id) + '\' doesn\'t exist in the List')
+            print("\33[31m\033[1m {}".format('Book with ID = \'' + str(book_id) + '\' doesn\'t exist in the List'))
         else:
             Book.book_list.pop(index_store)
             print('Book with ID = \'' + str(book_id) + '\' has been removed from the List!')
@@ -108,33 +108,33 @@ class Book:
             for i in range(len(Book.book_list)):
                 print(Book.book_list[i])
         else:
-            print('List of book is empty!')
+            print("\33[31m\033[1m {}".format('List of book is empty!'))
 
     @classmethod
     def find_book_by_author(cls, author_name):
         checker = 0
         for i in range(len(cls.book_list)):
-            if cls.book_list[i].book_author.casefold() == author_name.casefold():
+            if cls.book_list[i].__book_author.casefold() == author_name.casefold():
                 print(cls.book_list[i])
                 checker = 1
         if checker == 0:
-            print('Book with Author Name = \'' + author_name + '\' has not been found!')
+            print("\33[31m\033[1m {}".format('Book with Author Name = \'' + author_name + '\' has not been found!'))
 
     @classmethod
     def find_book_by_year_greater(cls, year):
         checker = 0
         for i in range(len(cls.book_list)):
-            if cls.book_list[i].book_year >= year:
+            if cls.book_list[i].__book_year >= year:
                 print(cls.book_list[i])
                 checker = 1
         if checker == 0:
-            print('No books have been found for provided conditions!')
+            print("\33[31m\033[1m {}".format('No books have been found for provided conditions!'))
 
     def __str__(self):
         return 'Book ID = ' + str(
-            self.book_id) + '; Book Name  = ' + self.book_name + '; Book Author = ' + self.book_author + '; Book Publishing House = ' + self.book_publishing_house + '; Book year = ' + str(
-            self.book_year) + '; Book Pages = ' + str(self.book_pages) + '; Book Price = ' + str(
-            self.book_price) + '; Book Binding = ' + self.book_binding_type
+            self.__book_id) + '; Book Name  = ' + self.__book_name + '; Book Author = ' + self.__book_author + '; Book Publishing House = ' + self.__book_publishing_house + '; Book year = ' + str(
+            self.__book_year) + '; Book Pages = ' + str(self.__book_pages) + '; Book Price = ' + str(
+            self.__book_price) + '; Book Binding = ' + self.__book_binding_type
 
 
 
@@ -164,7 +164,7 @@ def create_new_book():
 
 option = 1
 while option != 0:
-    print('\nList of options. Please select one')
+    print("\33[0m\033[1m {}".format('\nList of options. Please select one'))
     print('1 - Add a book')
     print('2 - Delete book')
     print('3 - List of books')
